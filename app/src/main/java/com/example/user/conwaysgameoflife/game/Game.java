6df28d595ceaaa572c0fda17c4202158e46b68fa
D.Naydenov <naidim@abv.bg>
2018-06-30 01:51:19 +0300

    squash:game

--- app/src/main/java/com/example/user/conwaysgameoflife/game/Game.java
+++ app/src/main/java/com/example/user/conwaysgameoflife/game/Game.java
@@ -7,42 +7,53 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
+import static java.util.Collections.emptySet;
+
 public class Game {
 
     private Population population;
 
-    private int speedLevel;
+    private Set<Point> selectedPoints;
+
     private boolean running;
+    private final int speedLevel;
+    private final int size;
 
     private int generationNumber;
 
-    public Game(final Collection<Point> alivePoints, final int gridSize, final int speedLevel) {
-        this.population = new Population(gridSize, new HashSet<>(alivePoints));
+    public Game(final int gridSize, final int speedLevel) {
+        this.selectedPoints = new HashSet<>();
+        this.size = gridSize;
         this.running = false;
         this.speedLevel = speedLevel;
     }
 
-    public void start() {
+    public synchronized void start() {
         running = true;
+        population = new Population(size, selectedPoints);
     }
 
-    public void stop() {
+    public synchronized void stop() {
         running = false;
     }
 
-    public void resume() {
+    public synchronized void resume() {
         running = true;
     }
 
-    public boolean isRunning() {
+    public synchronized boolean isRunning() {
         return running;
     }
 
-    public int getGenerationNumber() {
+    public synchronized int getGenerationNumber() {
         return generationNumber;
     }
 
-    public Collection<Point> nextGeneration() {
+    public synchronized Collection<Point> nextGeneration() {
+        if (!population.isAlive()) {
+            stop();
+            return emptySet();
+        }
         final Population newPopulation = population.nextGeneration();
         population = newPopulation;
         Set<Point> visible = newPopulation.getVisible();
@@ -52,7 +63,7 @@ public class Game {
         return visible;
     }
 
-    public int getInterval() {
+    public synchronized int getInterval() {
         return speedLevel * 10;
     }
 }
