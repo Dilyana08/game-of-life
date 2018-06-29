69ba316f23764d0a968b8224c8425ad90b457632
D.Naydenov <naidim@abv.bg>
2018-06-29 22:01:12 +0300

    Implemented layout portion of the play activity screen.

--- app/src/main/java/com/example/user/conwaysgameoflife/PlayActivity.java
+++ app/src/main/java/com/example/user/conwaysgameoflife/PlayActivity.java
@@ -1,21 +1,31 @@
 package com.example.user.conwaysgameoflife;
 
-import android.graphics.PixelFormat;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
-import android.widget.GridView;
-import android.widget.ListAdapter;
+import android.widget.TableLayout;
+
+import com.example.user.conwaysgameoflife.grid.GridCreator;
 
 public class PlayActivity extends AppCompatActivity {
+
+    public static final int ROWS_OFFSET = 1000;
+    public static final int CELLS_OFFSET = 10000;
+
+    public final int GRID_ID = R.id.grid;
+
+    private final GridCreator presenter;
+
+    public PlayActivity() {
+        presenter = new GridCreator();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
 
-        GridView gridview = findViewById(R.id.grid);
-       // gridview.setAdapter((ListAdapter) new PixelFormat());
-
+        final TableLayout grid = findViewById(GRID_ID);
+        presenter.createGrid(grid, 8, R.color.colorBlack);
     }
 
 }
---
+++ app/src/main/java/com/example/user/conwaysgameoflife/grid/GridCreator.java
@@ -0,0 +1,57 @@
+package com.example.user.conwaysgameoflife.grid;
+
+import android.content.Context;
+import android.view.Gravity;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+
+import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
+import static android.widget.LinearLayout.HORIZONTAL;
+import static com.example.user.conwaysgameoflife.PlayActivity.CELLS_OFFSET;
+import static com.example.user.conwaysgameoflife.PlayActivity.ROWS_OFFSET;
+
+public class GridCreator {
+
+    public void createGrid(final TableLayout grid, final int size, final int backgroundColour) {
+        final Context gridContext = grid.getContext();
+
+        for (int row = 0; row < size; row++) {
+            final LinearLayout rowLayout = createRow(row + ROWS_OFFSET, gridContext);
+            final Context rowContext = rowLayout.getContext();
+            for (int column = 0; column < size; column++) {
+                final int buttonId = CELLS_OFFSET + rowLayout.getId() + column;
+                final Button cellButton = createCell(buttonId, rowContext, backgroundColour);
+                rowLayout.addView(cellButton);
+            }
+            grid.addView(rowLayout);
+        }
+    }
+
+    private LinearLayout createRow(final int id, final Context context) {
+        final LinearLayout layout = new LinearLayout(context);
+        layout.setId(id);
+        layout.setOrientation(HORIZONTAL);
+
+        final TableLayout.LayoutParams layoutParams = new TableLayout.LayoutParams(0, WRAP_CONTENT, 1.0f);
+        layoutParams.gravity = Gravity.CENTER;
+
+        layout.setLayoutParams(layoutParams);
+
+        return layout;
+    }
+
+    private Button createCell(final int id, final Context context, int backgroundColour) {
+        final LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(0, MATCH_PARENT, 1.0f);
+        layoutParams.setMargins(1, 1, 1, 1);
+
+        final Button button = new Button(context);
+        button.setId(id);
+
+        button.setLayoutParams(layoutParams);
+        button.setBackgroundColor(backgroundColour);
+
+        return button;
+    }
+}
