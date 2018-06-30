35595ecb601ef01cf73b579e95a2b146f6c95c33
Dilyana Gineva <dilyana_vp@abv.bg>
2018-06-30 04:51:01 +0300

    ButtonUtils modified;

--- app/src/main/java/com/example/user/conwaysgameoflife/utils/ButtonUtils.java
+++ app/src/main/java/com/example/user/conwaysgameoflife/utils/ButtonUtils.java
@@ -1,5 +1,6 @@
 package com.example.user.conwaysgameoflife.utils;
 
+import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.widget.Button;
 
@@ -7,14 +8,16 @@ import com.example.user.conwaysgameoflife.R;
 
 public class ButtonUtils {
 
-    public static void setDefaultButtonColours(final Button button, Resources resources) {
-        button.setBackgroundColor(resources.getColor(R.color.colorMain));
+    public static void setDefaultButtonColours(final Button button, Resources resources, final SharedPreferences settings) {
+        int colorId = ColorUtils.getColorId(settings);
+        button.setBackgroundColor(resources.getColor(colorId));
         button.setTextColor(resources.getColor(R.color.colorBlack));
     }
 
-    public static void setActiveButtonColours(final Button button, Resources resources) {
+    public static void setActiveButtonColours(final Button button, Resources resources, final SharedPreferences settings) {
+        int colorId = ColorUtils.getColorId(settings);
         button.setBackgroundColor(resources.getColor(R.color.colorBlack));
-        button.setTextColor(resources.getColor(R.color.colorMain));
+        button.setTextColor(resources.getColor(colorId));
     }
 
     public static void drawGridButton(Button button, boolean active, Resources resources) {
